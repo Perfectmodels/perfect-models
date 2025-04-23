d11e066a12202a5f8745cb24ec463ef1132e926a
Perfectmodels <203891273+Perfectmodels@users.noreply.github.com>
2025-04-23 21:53:47 +0200

    Update Testimonials.tsx

--- src/components/home/Testimonials.tsx
+++ src/components/home/Testimonials.tsx
@@ -12,13 +12,13 @@ const testimonials = [
   {
     name: "Edele A.",
     role: "Styliste Modeliste",
-    image: "https://i.ibb.co/BV1HFbft/edele-a.jpg",
+    image: "https://scontent.flbv4-1.fna.fbcdn.net/v/t39.30808-6/489946152_122223850502231407_7759825436890322704_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeEnQAu2_j7HbZjiNeC6pWiXjyZpFwoMlNKPJmkXCgyU0vNZvGfS2rq4ZBN1xiLBoaCf3pMhVuEU-rL73gd3t31E&_nc_ohc=WnFBuxr_89AQ7kNvwHwsEhQ&_nc_oc=AdlcdCb70GlLHy8UUQztuw8hS8pX8nRFfZkZNvU3SrZQ8vrVZCVRopCGglpFCdd2V7w&_nc_zt=23&_nc_ht=scontent.flbv4-1.fna&_nc_gid=JpPUSSMGd3RR6OXZAp9JCA&oh=00_AfFChscKP1JyX8y2HI2CKZ_c29Sip1-e0c9fqSAyO0zzPA&oe=680F26A4",
     quote: "Perfect Models Management offre un service de qualité exceptionnelle. Leur professionnalisme et leur attention aux détails sont incomparables."
   },
   {
     name: "NR Pictures",
     role: "Studio Photo",
-    image: "https://i.ibb.co/yc1fYqB/NR.jpg",
+    image: "https://scontent.flbv4-1.fna.fbcdn.net/v/t39.30808-6/348590633_273972625048821_6724562378134660531_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeEjyJh0UcVEpU3NjQ7od3Xl4bhATVQxVifhuEBNVDFWJ0CP5niCPiuC-yHB2jeBZmgSwTZrZbrn0NXEZRQDaia6&_nc_ohc=qZJOoT2UQLsQ7kNvwGSMyM6&_nc_oc=AdnvENrD_9fLI2WI8dg9tYNVkRbBKaJe_Jl_joUQp3alhKgWbgKQvb5uFGMYSdXCoDQ&_nc_zt=23&_nc_ht=scontent.flbv4-1.fna&_nc_gid=t0tIhXtyjp5s8NWwsRdV6Q&oh=00_AfE4JgIITO7750RZn0yC00DHUNCW_iU5YymsEzD3fY3BMQ&oe=680F1456",
     quote: "PMM offre une expérience photographique exceptionnelle. Leur créativité et leur professionnalisme capturent des moments inoubliables."
   },
   {
