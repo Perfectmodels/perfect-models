5c5e0ac1f7e99da4cdc494569e9af33c9825d8b9
Perfectmodels <203891273+Perfectmodels@users.noreply.github.com>
2025-04-23 22:44:10 +0200

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -10,38 +10,6 @@ import EventsList from '../components/models/EventsList';
 import { Separator } from '@/components/ui/separator';
 import type { Service, Event } from '../types/modelTypes';
 
-const services: Service[] = [
-  {
-    id: "1",
-    name: "Mannequinat",
-    description: "Formation et placement de mannequins professionnels",
-    image: "https://images.pexels.com/photos/1699161/pexels-photo-1699161.jpeg"
-  },
-  {
-    id: "2",
-    name: "Défilés de Mode",
-    description: "Organisation et participation à des défilés de mode",
-    image: "https://images.pexels.com/photos/1926769/pexels-photo-1926769.jpeg"
-  },
-  {
-    id: "3",
-    name: "Shooting Photo",
-    description: "Séances photo professionnelles pour books et campagnes",
-    image: "https://images.pexels.com/photos/2811087/pexels-photo-2811087.jpeg"
-  }
-];
-
-const events: Event[] = [
-  {
-    id: "1",
-    name: "Perfect Fashion Day",
-    description: "Notre événement annuel de mode",
-    date: "Janvier 2025",
-    location: "Libreville",
-    image: "https://i.ibb.co/LDzx7JgX/470506390-575121368705542-822202287511994230-n.jpg"
-  },
-
-];
 
 const About = () => {
   return (
