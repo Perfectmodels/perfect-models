7da7d4cb947708ba7ca6642361605fe0bb773e33
Perfectmodels <203891273+Perfectmodels@users.noreply.github.com>
2025-04-23 19:03:39 +0200

    Update Casting.tsx

--- src/pages/Casting.tsx
+++ src/pages/Casting.tsx
@@ -4,28 +4,6 @@ import Footer from '../components/layout/Footer';
 import CastingForm from '@/components/casting/CastingForm';
 import SuccessDialog from '@/components/casting/SuccessDialog';
 
-const modelCategories = [
-  { id: "1", name: "Mannequin de défilé / Runway model" },
-  { id: "2", name: "Mannequin photo / Fashion model" },
-  { id: "3", name: "Mannequin commercial / Commercial model" },
-  { id: "4", name: "Mannequin lingerie & maillot / Lingerie & swimsuit model" },
-  { id: "5", name: "Mannequin showroom" },
-  { id: "6", name: "Mannequin artistique / Art model" },
-  { id: "7", name: "Mannequin de détails / Parts model" },
-  { id: "8", name: "Mannequin publicitaire / Advertising model" },
-  { id: "9", name: "Mannequin haute couture" },
-  { id: "10", name: "Mannequin petite taille / Petite model" },
-  { id: "11", name: "Mannequin grande taille / Plus-size model" },
-  { id: "12", name: "Mannequin fitness / Fitness model" },
-  { id: "13", name: "Mannequin senior / Mature model" },
-  { id: "14", name: "Mannequin enfant / Child model" },
-  { id: "15", name: "Mannequin adolescent / Teen model" },
-  { id: "16", name: "Mannequin influenceur" },
-  { id: "17", name: "Mannequin alternatif / Alternative model" },
-  { id: "18", name: "Mannequin ethnique / Ethnic model" },
-  { id: "19", name: "Mannequin androgynes" },
-]
-
 const Casting = () => {
   const [showSuccessDialog, setShowSuccessDialog] = useState(false);
 
@@ -52,7 +30,7 @@ const Casting = () => {
               </p>
             </div>
 
-            <CastingForm onSuccess={handleFormSuccess} modelCategories={modelCategories} />
+            <CastingForm onSuccess={handleFormSuccess} />
 
             <SuccessDialog
               open={showSuccessDialog}
